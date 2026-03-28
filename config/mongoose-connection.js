06b64ffd9de37dd3fab764a45d304f260a941438
Somya-agrawal659 <agrawalsomya659@gmail.com>
2026-03-28 16:55:41 +0530

    Revert "Configure for Vercel serverless with MongoDB session store"
    This reverts commit 289f31249cce4bb93db3e319dc17eae00cda8ca3.

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -4,30 +4,23 @@ const dbgr = require("debug")("development:mongoose")
 
 const config = require('config')
 
-// Serverless-friendly connection options
-const connectionOptions = {
-  maxPoolSize: 10,              // Limit connection pool for serverless
-  serverSelectionTimeoutMS: 5000,
-  socketTimeoutMS: 45000,
-};
+mongoose
 
-// Prevent duplicate connections in serverless environment
-if (mongoose.connection.readyState === 0) {
-  mongoose
-    //pehle mongoose connect krne bolega
-    //yeh waala sirf local server connect krne k liye hota hai
-    .connect(`${config.get("MONGODB_URI")}/scatch`, connectionOptions)
-    
-    //agr connect hogya then
-    .then(function(){
-        dbgr("connected")
-    })
-    
-    //agar connect nahi ho rha hai
-    .catch(function(err){
-        dbgr(err);                                   
-    })
-}
+
+
+//pehle mongoose connect krne bolega
+//yeh waala sirf local server connect krne k liye hota hai
+.connect(`${config.get("MONGODB_URI")}/scatch`)
+
+//agr connect hogya then
+.then(function(){
+    dbgr("connected")
+})
+
+//agar connect nahi ho rha hai
+.catch(function(err){
+    dbgr(err);                                   
+})
 
 // to run the server and connection then we will export the model
 module.exports = mongoose.connection
