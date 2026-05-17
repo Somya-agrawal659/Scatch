47872c6768c3cbc7df3afc5fb446ce346f2fdb5e
Somya-agrawal659 <agrawalsomya659@gmail.com>
2026-05-17 14:53:19 +0530

    fix vercel deployment

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
-const mongoose = require('mongoose');
 
-const dbUri = process.env.MONGODB_URI || "mongodb+srv://Somya:Somya@interview-ai-cluster.6314tk0.mongodb.net/scatch?retryWrites=true&w=majority&appName=interview-ai-cluster";
+const mongoose = require("mongoose");
 
-mongoose.connect(dbUri)
-.then(()=>console.log("Connected"))
-.catch(err=>console.log(err));
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 // to run the server and connection then we will export the model
 module.exports = mongoose.connection
