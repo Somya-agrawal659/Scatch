1cf52a253f4e3a9a19c0773c7a2e60c0ab2fb207
Somya-agrawal659 <agrawalsomya659@gmail.com>
2026-05-17 15:05:50 +0530

    fix view path for vercel

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 //ejs ka view engine setup kiya
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use("/",indexRouter);
